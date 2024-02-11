233edc1e3d800794a2517eab4ae9d89fda5e41e9
Alexander Ershov <yearshow@yandex.ru>
2024-02-11 19:38:31 +0300

    Courses list

---
+++ src/features/courses-list/model/types.ts
@@ -0,0 +1,16 @@
+type CourseListElement = {
+    id: string;
+    name: string;
+    description: string;
+}
+
+type CreateCourseListElementCommand = {
+    name: string;
+    description: string;
+
+}
+
+type DeleteCourseListElementCommand = {
+    id: string;
+}
+
